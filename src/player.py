a3c07beb47a8f1d117eacabcd419edb8f62dd34e
Hunter Stevens <hunterstevens1996@gmail.com>
2020-06-03 13:40:40 -0600

    created classes for player and room

--- src/player.py
+++ src/player.py
@@ -1,2 +1,7 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+    def __init__(self, move, currentRoom='outside'):
+        self.currentRoom = currentRoom
+        self.move = move
